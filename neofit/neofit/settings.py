de5341c63a7e1ea36bf03788b7ba9f38b7f53812
Azamat.Jumagulov <djuma94@gmail.com>
2022-07-26 18:04:27 +0600

    Третий коммит, возможно проблема с БД, не делаются миграции

--- neofit/neofit/settings.py
+++ neofit/neofit/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'neofit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\azamat.jumagulov\Desktop\Раб стол 09-02-2022\Python_project_1\projects\neofit\neofit\news\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
