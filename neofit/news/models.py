98f50d2cf571613b539fc563ee53362892ad90e3
Azamat.Jumagulov <djuma94@gmail.com>
2022-08-01 13:01:38 +0600

    нужно передать или создать по новой записи

--- neofit/news/models.py
+++ neofit/news/models.py
@@ -13,6 +13,9 @@ class Articles(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
